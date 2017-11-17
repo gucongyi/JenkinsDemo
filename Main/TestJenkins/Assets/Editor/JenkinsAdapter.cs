143ed765c5f2913af06b22d27d51a34d2c377dea
MengKaiYu <1466705180@qq.com>
2017-11-17 18:20:56 +0800

    test

---
+++ Main/TestJenkins/Assets/Editor/JenkinsAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class JenkinsAdapter : Editor {
+
+    [MenuItem("Jenkins/JenkinsBuild")]
+    public static void Build()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+
+        List<string> sceneList = new List<string>();
+        EditorBuildSettingsScene[] temp = EditorBuildSettings.scenes;
+        for (int i = 0, iMax = temp.Length; i < iMax; ++i)
+            sceneList.Add(temp[i].path);
+
+        BuildPipeline.BuildPlayer(sceneList.ToArray(), "./android.apk", BuildTarget.Android, BuildOptions.None);
+    }
+}
