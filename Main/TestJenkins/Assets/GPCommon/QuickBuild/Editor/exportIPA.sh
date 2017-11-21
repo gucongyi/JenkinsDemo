be5bbaf7b1d03e36cb53ce68a98f37b2d560a391
g01 <hyz@qq.com>
2017-11-21 19:05:04 +0800

    sh添加钥匙串控制命令

--- Main/TestJenkins/Assets/GPCommon/QuickBuild/Editor/exportIPA.sh
+++ Main/TestJenkins/Assets/GPCommon/QuickBuild/Editor/exportIPA.sh
@@ -10,6 +10,8 @@ echo ${archiveName}
 
 cd ${path}
 xcodebuild clean
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/login.keychain-db
+security unlock-keychain -p "123456" ~/Library/Keychains/login.keychain-db
 xcodebuild archive -scheme "Unity-iPhone" -configuration "Release" -archivePath ${archiveName}
 xcodebuild -exportArchive -archivePath ${archiveName} -exportPath ${name} -exportOptionsPlist Info.plist
 
